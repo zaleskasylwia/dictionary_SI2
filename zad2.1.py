8da445e258bfee903a5694a3e949ebe42de838d9
sylwia <zaleska.syl@gmail.com>
2017-04-14 15:18:24 +0200

    Wersja bez joina, dodajaca dobrze do pliku

--- zad2.1.py
+++ zad2.1.py
@@ -1,22 +1,44 @@
 import csv
+'''dictionary = {"FUNCTION": ("A series of statements which returns some value to a caller. ", "https://docs.python.org/3/glossary.html#term-function"),
+                 "PARAMETR": ("A named entity in a function definition that specifies an argument that the function can accept", "https://docs.python.org/3/glossary.html#term-parameter"),
+                 "VARIABLE": (" A variable is a location in memory used to store some data (value). ", "https://www.programiz.com/python-programming/variables-datatypes"), 
+                 "ARGUMENT": ("A value passed to a function (or method) when calling the function", "https://docs.python.org/3/glossary.html#term-argument"), 
+                "DICTIONARY": ("An associative array where arbitrary keys are mapped to values.", "https://docs.python.org/3/glossary.html#term-dictionary"), 
+                "TUPLE": ("Object similar to a list. The difference between the two is that we cannot change the elements of a tuple once it is assigned whereas in a list  elements can be changed.", "https://www.programiz.com/python-programming/tuple"), 
+                "ASCII table": ("ASCII stands for American Standard Code for Information Interchange. ASCII code is the numerical representation of a characters.", "http://www.asciitable.com/"),
+                "MODULE": ("An object that serves as an organizational unit of Python code", "https://docs.python.org/3/glossary.html#term-module"),
+                "LIST": ("A built-in Python sequence.", "https://docs.python.org/3/glossary.html#term-list"),
+                "STRING": (" A string is a sequence of characters.", "https://www.programiz.com/python-programming/string"),
+                "METHOD": ("A function which is defined inside a class body.", "https://docs.python.org/3/glossary.html#term-method"),
+                "LOOP": ("Loops are used in programming to repeat a specific block of code.", "https://www.programiz.com/python-programming/while-loop"),
+                "IMMUTABLE": ("An object with a fixed value. Immutable objects include numbers strings and tuples.", "https://docs.python.org/3/glossary.html#term-immutable"),
+                "ITERABLES": ("An object capable of returning its members one at a time.", "https://docs.python.org/3/glossary.html#term-iterable"),
+                "VCS": ("A component of software configuration management version control system also known as revision control or source control", "https://en.wikipedia.org/wiki/Version_control")}'''
 
-dictionary = {"FUNCTION": ("A series of statements which returns some value to a caller. ", "https://docs.python.org/3/glossary.html#term-function"),
-                 "PARAMETR": ("co to parametr", "zrodlo p"),"VARIABLE": ("co to variable", "zrodlo v"), "ARGUMENT": ("co to argument", "zrodlo a"), 
-                "DICTIONARY": ("co to dictionary", "zrodlo d"), "TUPLE": ("co to tuple", "zrodlo t"), 
-                "ASCII table": ("co to ascii", "zrodlo asc"),"MODULE": ("co to module", "zrodlo m"),
-                "LIST": ("co to list", "zrodlo l"),"CONDITIONAL": ("co to conditional", "zrodlo c"),
-                "LOOPS": ("co to loops", "zrodlo lo"),"BUILT-IN": ("co to built-in", "zrodlo b"),
-                "SLOTS": ("co to slots", "zrodlo s"),"GENERATORS": ("co to generators", "zrodlo g"),
-                "CLASSES": ("co to classes", "zrodlo cl")}
-
-with open('proba3.csv', 'w') as f:  #tworzy nowy plik i wpisuje zawartosc slownika
-    w = csv.writer(f)
+'''with open('data.csv', 'w') as csv_file:  #tworzy nowy plik i wpisuje zawartosc slownika
+    w = csv.writer(csv_file)
     for key, val in dictionary.items():
-        w.writerow([key, val])
+        w.writerow([key, val])'''
 
 
-#add = {}    
-    
+
+
+
+with open('data.csv', 'r') as csv_file:
+    reader = csv.reader(csv_file)
+    dictionary = dict(reader)
+
+#1
+def search():
+    print("Enter appellation of the definition")
+    name = input().upper()
+    if dictionary.get(name):
+        print(dictionary.get(name))
+    else:
+        print("Dictionary don't have this definition")
+
+
+
 
 #1
 def search():
@@ -36,11 +58,11 @@ def adding():
     print("Tell me source")
     source = input()
     dictionary[definition] = (explanation, source) #dodaje do slownika moja nowa definicje
-    print(dictionary)
-    with open('proba3.csv', 'w') as f:  #tworzy nowy plik i wpisuje zawartosc slownika
-        w = csv.writer(f)
-        for key, val in dictionary.items():
-            w.writerow([key, val])
+    #print(dictionary)
+    with open('data.csv', 'w') as csv_file:                        
+        writer = csv.writer(csv_file)
+        for key, value in dictionary.items():
+            writer.writerow([key, value])
 
 
 #3
